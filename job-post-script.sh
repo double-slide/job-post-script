6f9fc65ae26d76b01afc6c72868cc6b3b07b54e0
Iaan Johnston <iaan.e.jo@gmail.com>
2022-06-23 01:51:31 -0700

    add csv for job posting urls, import csv

--- job-post-script.sh
+++ job-post-script.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo 'Hello';
+JOB_INPUT = job_site_data.csv
+
+echo 'Initializing job posting word search...';
+
+wget -qO- https://www.avalanche.ca/careers \
+| grep -q "sunshine" \
+&& echo "Found Avalanche Forecaster"
+
+echo 'Job posting word search complete.';
