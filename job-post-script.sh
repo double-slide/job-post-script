e9a07eff89830d2f6b2c6650f69cc67c455e4d27
Iaan Johnston <iaan.e.jo@gmail.com>
2022-06-23 02:12:45 -0700

    Import CSV file contents to bash script

--- job-post-script.sh
+++ job-post-script.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-JOB_INPUT = job_site_data.csv
+JOBSITES=job_site_data.csv
 
-echo 'Initializing job posting word search...';
+# echo $JOB_INPUT;
+
+echo 'Initializing job posting word search...'
+ 
+IFS=','
+
+# [ ! -f $JOBSITES ] && {echo "$JOBSITES file not found"; exit 99; }
+
+while IFS=, read -r col1 
+do
+	echo "$col1"
+	echo "<br>"
+done < $JOBSITES
+
+
+
 
 wget -qO- https://www.avalanche.ca/careers \
 | grep -q "sunshine" \
